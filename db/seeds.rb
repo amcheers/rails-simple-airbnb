44403bd720c5715a38d2fcd11c254f6b4a4783fa
Ahmad Cheers <acheers1@gmail.com>
2020-11-18 22:11:38 +0100

    Coded index action in controller, seeded db with 1 valid flat instance and checked model validations, basic formatting of flats#index view.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create(name: 'Cute bungalow for 2', address: '123 main lane', description: 'A quaint house great for a weekend getaway', 
+price_per_night: 500, number_of_guests: 2)
